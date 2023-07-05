d2468d43873b511732bf1ac14118c52b80a86a55
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-07-05 12:57:48 -0600

    First Java Program

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Chelsey Campbell");
+        System.out.println("I am 38 years old");
+        System.out.println("My hometown is Blackfoot, ID");
+    }
+}
+
